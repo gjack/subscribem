69ba1f003bf1fb10dbae475b0645e02eed1bddf8
gjack <gabi@gabijack.com>
2015-12-01 21:09:18 -0800

    finish sign in

--- app/models/subscribem/account.rb
+++ app/models/subscribem/account.rb
@@ -11,5 +11,13 @@ module Subscribem
     accepts_nested_attributes_for :owner
     has_many :members, class_name: "Subscribem::Member"
     has_many :users, through: :members
+    
+    def self.create_with_owner(params ={})
+      account = new(params)
+      if account.save
+        account.users << account.owner
+      end
+      account
+    end
   end
 end
